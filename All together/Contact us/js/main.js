69e85c71a9fd7a1f9f610a94ea19b749d5b2f245
aryalamit0011dragon <77341032+aryalamit0011dragon@users.noreply.github.com>
2021-01-13 22:33:11 +0545

    my final website

---
+++ All together/Contact us/js/main.js
@@ -0,0 +1,102 @@
+
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+    
+    /**click event on toggle menu */
+    $toggleCollapse.click(function(){
+    
+        $nav.toggleClass('collapse');
+    })
+    
+    //owl-carousel for blog//
+    $('.owl-carousel').owlCarousel({
+    
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        dots:false,
+        nav:true,
+        navText : [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
+    //click to scroll up//
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+    
+       }, 1000);
+    
+    })
+    
+    
+    });
+
+
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
+})(jQuery);
